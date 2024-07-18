15cbd35b1024c3c259dc894d194213ba063afb0f
Mayank Bhardwaj <mayank.bhardwaj@mechademy.com>
2024-07-17 21:30:48 -0400

    update: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
-// Import the 'express' module along with 'Request' and 'Response' types from express
-import express, { Request, Response } from 'express';
+import { config } from "dotenv";
+import connectDB from "./config/database";
+import express from "express";
 
-// Create an Express application
+config();
 const app = express();
 
-// Specify the port number for the server
-const port: number = 4000;
+// Middleware
+app.use(express.json());
 
-// Define a route for the root path ('/')
-app.get('/', (req: Request, res: Response) => {
-  // Send a response to the client
-  res.send('Hello, TypeScript + Node.js + Express!');
+// Routes
+//app.use('/api/users', userRoutes);
+
+// Connect to MongoDB
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-// Start the server and listen on the specified port
-app.listen(port, () => {
-  // Log a message when the server is successfully running
-  console.log(`Server is running on http://localhost:${port}`);
-});
+export default app;
