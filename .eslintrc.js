fbed198c5499e200b7c787de4503f2b9185348fa
Mayank Bhardwaj <mayank071293@gmail.com>
2024-07-19 22:39:53 -0400

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: [
+      '@typescript-eslint',
+      'prettier',
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      // Add any other rules you'd like to enforce
+    },
+  };
+  
