83cce2f7e3e3d031a898ec89d2cc6573389ee947
Mayank Bhardwaj <mayank071293@gmail.com>
2024-07-19 22:08:56 -0400

    add: schmas for LRWS of tests

---
+++ src/models/ListeningExam.ts
@@ -0,0 +1,41 @@
+import { Document, Schema, model } from "mongoose";
+
+interface ListeningQuestion {
+  statement: string;
+  options: string[];
+}
+
+const ListeningQuestionSchema = new Schema<ListeningQuestion>({
+  statement: { type: String, required: true },
+  options: { type: [String], required: true },
+});
+
+interface ListeningQuestionSet {
+  audio: string; //url
+  paragraph: string;
+  questions: ListeningQuestion[];
+}
+
+const ListeningQuestionSetSchema = new Schema<ListeningQuestionSet>({
+  audio: { type: String },
+  paragraph: { type: String, required: true },
+  questions: { type: [ListeningQuestionSchema], required: true },
+});
+
+// Define a TypeScript interface for the ListeningExam document
+interface ListeningExam extends Document {
+  question_sets: ListeningQuestionSet[];
+}
+
+// Create a Mongoose schema based on the ListeningExam interface
+const ListeningExamSchema = new Schema<ListeningExam>({
+  question_sets: { type: [ListeningQuestionSetSchema], required: true },
+});
+
+// Create a Mongoose model based on the schema
+const ListeningExam = model<ListeningExam>(
+  "ListeningExam",
+  ListeningExamSchema
+);
+
+export default ListeningExam;
---
+++ src/models/WritingExam.ts
@@ -0,0 +1,16 @@
+import { Document, Schema, model } from "mongoose";
+
+// Define a TypeScript interface for the WritingExam document
+interface WritingExam extends Document {
+  question_sets: [String];
+}
+
+// Create a Mongoose schema based on the WritingExam interface
+const WritingExamSchema = new Schema<WritingExam>({
+  question_sets: { type: [String], required: true },
+});
+
+// Create a Mongoose model based on the schema
+const WritingExam = model<WritingExam>("WritingExam", WritingExamSchema);
+
+export default WritingExam;
