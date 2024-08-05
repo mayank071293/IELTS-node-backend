11500644c10e02742bce3ecc6d01518b62313fa0
Mayank Bhardwaj <mayank071293@gmail.com>
2024-08-04 23:39:57 -0400

    add: answerKeys model

---
+++ src/models/answerKey.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, ObjectId, Schema, model } from "mongoose";
+interface Answer {
+  question_number: string;
+  explanation: string;
+  options: string[];
+}
+
+const AnswerSchema = new Schema<Answer>({
+  question_number: { type: String, required: true },
+  explanation: { type: String },
+  options: { type: [String], required: true },
+});
+
+interface AnswerKey extends Document {
+  exam_id: ObjectId;
+  answers: Answer[];
+}
+
+const AnswerKeySchema = new Schema<AnswerKey>({
+  exam_id: { type: mongoose.Types.ObjectId, required: true },
+  answers: { type: [AnswerSchema], required: true },
+});
+
+const AnswerKey = model<AnswerKey>("AnswerKey", AnswerKeySchema);
+
+export { AnswerKey, Answer };
