d16346212efa1dd0b635b14b2c7e592f104d7b5c
Mayank Bhardwaj <mayank071293@gmail.com>
2024-08-05 18:21:26 -0400

    update: creating index on exam_id in AnswerKey model

--- src/models/answerKey.ts
+++ src/models/answerKey.ts
@@ -17,9 +17,11 @@ interface AnswerKey extends Document {
 }
 
 const AnswerKeySchema = new Schema<AnswerKey>({
-  exam_id: { type: mongoose.Types.ObjectId, required: true },
+  exam_id: { type: mongoose.Types.ObjectId, required: true, unique: true },
   answers: { type: [AnswerSchema], required: true },
 });
+
+AnswerKeySchema.index({ exam_id: 1 }, { unique: true });
 
 const AnswerKey = model<AnswerKey>("AnswerKey", AnswerKeySchema);
 
