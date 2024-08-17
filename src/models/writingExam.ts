0a3f25bc122eeca131325f4f4148442708b08e24
Mayank Bhardwaj <mayank071293@gmail.com>
2024-08-16 23:50:32 -0400

    update: writingExam schema validation

--- src/models/writingExam.ts
+++ src/models/writingExam.ts
@@ -7,7 +7,16 @@ interface WritingExam extends Document {
 
 // Create a Mongoose schema based on the WritingExam interface
 const WritingExamSchema = new Schema<WritingExam>({
-  question_sets: { type: [String], required: true },
+  question_sets: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: function (value: string[]) {
+        return value.length === 2;
+      },
+      message: "The question_sets array must contain exactly 2 items.",
+    },
+  },
 });
 
 // Create a Mongoose model based on the schema
